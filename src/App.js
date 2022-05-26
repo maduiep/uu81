4986307339dbcb3d30706f0845ff3da76c60b2ad
Babajide Tomoshegbo <babajide234@gmail.com>
2022-05-26 22:19:31 +0100

    more routes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ import Home from './component/Home';
 import Register from './component/Auth/Register';
 import Login from './component/Auth/Login';
 import Layout from './component/Layout';
+import DashLayout from './component/Dashboard/DashLayout';
+import DashHome from './component/Dashboard/DashHome';
+import Bookings from './component/Dashboard/Bookings';
+import Payments from './component/Dashboard/Payments';
+import Profile from './component/Dashboard/Profile';
+import Event from './component/Event';
 
 function App() {
   const {accessToken} = useSelector((state) =>  state.auth);
@@ -27,8 +33,15 @@ function App() {
     <Routes>
         <Route path="/" element={<Layout />} >
           <Route path="/" element={<Home/>} />
+          <Route path="/:id" element={<Event/>} />
           <Route path="register" element={<Register/>} />
           <Route path="login" element={<Login/>} />
+        </Route>
+        <Route path='dashboard' element={<DashLayout/>}>
+          <Route path="dashboard" element={<DashHome/>} />
+          <Route path="bookings" element={<Bookings/>} />
+          <Route path="payments" element={<Payments/>} />
+          <Route path="payments" element={<Profile/>} />
         </Route>
     </Routes>
   );
---
+++ src/component/Dashboard/DashLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const DashLayout = () => {
+  return (
+    <>
+        <Outlet/>
+    </>
+  )
+}
+
+export default DashLayout
