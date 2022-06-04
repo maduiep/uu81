9310f34c96f0e3d650f4d5b875f4a30655c92916
Babajide Tomoshegbo <babajide234@gmail.com>
2022-06-04 13:27:39 +0100

    navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React,{ useState, useRef, useEffect } from 'react'
 
 const Navbar = () => {
+  
+const [navBackground, setNavBackground] = useState('navbar-transparent')
+const navRef = useRef()
+
+navRef.current = navBackground
+
+const handleScroll = () => {
+  const show = window.scrollY > 3;
+  console.log(show);
+
+  if (show) {
+      setNavBackground('navbar-color fixed ')
+  } else {
+      setNavBackground('navbar-transparent')
+  }
+}
+
+useEffect(() => {
+    document.addEventListener('scroll', handleScroll)
+
+    return () => {
+        document.removeEventListener('scroll', handleScroll)
+    }
+}, [])
+
   return (
     <>
       <div className="main">
-        <header className="header">
-          <nav className="navbar navbar-expand-lg">
+        <header className='header'>
+          <nav className={`navbar navbar-expand-lg ${ navRef.current }`}>
             <div className="container-fluid d-flex justify-content-around">
               <div className='brand d-flex justify-content-between align-items-center'>
                 <img src="/assets/uu81_logo1.png" alt="LOGO" width="40"/>
