529f2c911666d48b8e825bfe6bb6e51f7c14d22f
Emmanuel Madu IP <emmanuelmaduip@gmail.com>
2022-06-04 12:42:32 +0100

    completed registration page

--- src/component/Auth/Register.jsx
+++ src/component/Auth/Register.jsx
@@ -1,19 +1,39 @@
 import React from 'react'
 
+
 const Register = () => {
   return (
-    <div className=''>
-        <div className="container">
-            <div className="form-container">
-                <form action="">
-                    <p>
-						<input type="text" placeholder="Name" name="name" id="name"/>
-						<input type="email" placeholder="Email" name="email" id="email"/>
-					</p>
-                </form>
-            </div>
+    <>
+     <div className="container">
+     <form className='d-flex justify-content-center flex-column mt-4'>
+        <div className="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
         </div>
-    </div>
+        &nbsp;
+        <div className="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+        </div>
+        &nbsp;
+        <div className="form-group">
+          <label for="exampleInputFirstname">First name</label>
+          <input type="firstname" className="form-control" id="exampleInputFirstname" aria-describedby="firstnameHelp" placeholder="Enter Firstname" />
+        </div>
+        &nbsp;
+        <div className="form-group">
+          <label for="exampleInputLastname">Last name</label>
+          <input type="Lastname" className="form-control" id="exampleInputLastname" aria-describedby="lastnameHelp" placeholder="Enter Lastname" />
+        </div>
+        &nbsp;
+        <div className="form-group">
+          <label for="exampleInput Phone number">Phone number</label>
+          <input type="phone" className="form-control" id="exampleInputPhone number" aria-describedby="Phone numberHelp" placeholder="Enter Phone number" />
+        </div>
+        <button type="submit" className="btn btn-primary mt-4 mb-4">Submit</button>
+      </form>
+     </div>
+    </>
   )
 }
 
