7231e10019a0d0ef26fefe66a4bcc0dc1ca81733
Emmanuel Madu IP <emmanuelmaduip@gmail.com>
2022-06-04 13:15:51 +0100

    completed registration and login forms

--- src/component/Auth/Register.jsx
+++ src/component/Auth/Register.jsx
@@ -2,36 +2,40 @@ import React from 'react'
 
 
 const Register = () => {
+  
   return (
     <>
      <div className="container">
-     <form className='d-flex justify-content-center flex-column mt-4'>
-        <div className="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-        </div>
-        &nbsp;
-        <div className="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-        </div>
-        &nbsp;
-        <div className="form-group">
-          <label for="exampleInputFirstname">First name</label>
-          <input type="firstname" className="form-control" id="exampleInputFirstname" aria-describedby="firstnameHelp" placeholder="Enter Firstname" />
-        </div>
-        &nbsp;
-        <div className="form-group">
-          <label for="exampleInputLastname">Last name</label>
-          <input type="Lastname" className="form-control" id="exampleInputLastname" aria-describedby="lastnameHelp" placeholder="Enter Lastname" />
-        </div>
-        &nbsp;
-        <div className="form-group">
-          <label for="exampleInput Phone number">Phone number</label>
-          <input type="phone" className="form-control" id="exampleInputPhone number" aria-describedby="Phone numberHelp" placeholder="Enter Phone number" />
-        </div>
-        <button type="submit" className="btn btn-primary mt-4 mb-4">Submit</button>
-      </form>
+     <h3 className='mt-4'> Membership Registration</h3>
+      <form className='d-flex justify-content-center flex-column mt-4'>
+          <div className="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+          </div>
+          &nbsp;
+          <div className="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+          </div>
+          &nbsp;
+          <div className="form-group">
+            <label for="exampleInputFirstname">First name</label>
+            <input type="firstname" className="form-control" id="exampleInputFirstname" aria-describedby="firstnameHelp" placeholder="Enter Firstname" />
+          </div>
+          &nbsp;
+          <div className="form-group">
+            <label for="exampleInputLastname">Last name</label>
+            <input type="Lastname" className="form-control" id="exampleInputLastname" aria-describedby="lastnameHelp" placeholder="Enter Lastname" />
+          </div>
+          &nbsp;
+          <div className="form-group">
+            <label for="exampleInput Phone number">Phone number</label>
+            <input type="phone" className="form-control" id="exampleInputPhone number" aria-describedby="Phone numberHelp" placeholder="Enter Phone number" />
+          </div>
+          &nbsp;
+          <small id="emailHelp" class="form-text text-muted color">Already have an account?</small>
+          <button type="submit" className="btn btn-primary mt-4 mb-4">Submit</button>
+        </form>
      </div>
     </>
   )
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
@@ -6,17 +7,18 @@ const Navbar = () => {
       <nav className="navbar navbar-expand-lg">
         <div className="container-fluid d-flex justify-content-around">
           <div className='brand d-flex justify-content-between align-items-center'>
+           <Link style={{textDecoration: 'none', color: 'gray'}} className='d-flex justify-content-between align-items-center' to="/">
             <img src="/assets/uu81_logo1.png" alt="LOGO" width="40"/>
-            <h6>UNIQUE UNIPORT81</h6>
+            <h6>UNIQUE UNIPORT81</h6></Link>
           </div>
           <div 
             className="" id="navbarSupportedContent">
             <form className="d-flex btn-style">
-              <button type="button" class="btn btn-outline-primary">
-                Register
+              <button type="button" class="btn btn-outline-primary btn-hover">
+               <Link style={{textDecoration: 'none', listStyleType: 'none'}} to='/register'> Register</Link>
               </button>
-              <button type="button" class="btn btn-outline-primary ms-3">
-                SignIn
+              <button type="button" class="btn btn-outline-primary ms-3 btn-hover">
+              <Link style={{textDecoration: 'none', listStyleType: 'none'}} to='/login'> SignIn</Link>
               </button>
             </form>
           </div>
