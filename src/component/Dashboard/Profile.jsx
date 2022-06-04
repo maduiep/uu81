aba23c591a2748c2a41594266c19bef691875537
Emmanuel Madu IP <emmanuelmaduip@gmail.com>
2022-06-04 13:36:56 +0100

    added dashboard

--- src/component/Dashboard/Profile.jsx
+++ src/component/Dashboard/Profile.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+
+    <div className="container">
+      <div className="profile-wrapper">
+        <h1>hello</h1>
+      </div>
+    </div>
   )
 }
 
